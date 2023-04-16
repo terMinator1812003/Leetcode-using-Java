6d4681d964cd8a8cc4d49d53e3a211ac1e795682
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-04-16 13:39:08 +0530

    Add files via upload

---
+++ LongestCommonPrefix14.java
@@ -0,0 +1,19 @@
+
+public class LongestCommonPrefix14 {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+            }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String strs[] = { "flower", "flow", "flight" };
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
